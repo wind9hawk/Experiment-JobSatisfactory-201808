27ba5c5150561d701e957dfaa14e05d0aa6f1bfc
windhawkgyang <gyang_wind@163.com>
2018-10-30 10:13:14 +0800

    Script for analyze the basic feature distribution of 30 insiders in CERT 5.2 of scenario 2.

--- PythonCode0/CERT5.2_Big5_OS_Feats-0.3.py
+++ PythonCode0/CERT5.2_Big5_OS_Feats-0.3.py
@@ -37,3 +37,168 @@ f_LDAP.close()
 # ['1 - Executive', '1 - Adminstration', '', ''],BBB0012,CWC0006,
 print '..<<将LDAP数据处理成规范数据格式>>..\n'
 print '..<<OS序列, 成员1，成员2，...成员N>>..\n\n'
+# 采用拼接的方法，重新处理LDAP序列
+# 先定位到]位置，将其中的','替换为':'，然后再与后一部分分片拼接
+# 定义一个全新的规范过的LDAP列表
+CERT52_LDAP_NOM = []
+for line in f_LDAP_lst[:]:
+    line_lst = line.strip('\n').strip(',').split(',')
+    if len(line_lst) < 2:
+        continue
+    index_0 = line.index(']')
+    # 定义临时储存[]部分的分片
+    ldap_fragment_0 = line[:index_0 + 1]
+    # 定义临时存储[]之后部分的分片
+    ldap_fragment_1 = line[index_0 + 1:]
+    # 分别以‘,’分割，重新组成列表
+    ldap_lst_0 = ldap_fragment_0.strip('[').strip(']').replace(',', ':').replace('\'', '')
+    ldap_lst_1 = ldap_fragment_1.strip('\n').strip(',')
+    # 定义本次处理要返回的单行LDAP数据
+    ldap_tmp = []
+    ldap_tmp.append(ldap_lst_0.strip('"'))
+    for usr in ldap_lst_1.split(','):
+        ldap_tmp.append(usr)
+    print '要返回的规范化ldap数据为： ', ldap_tmp, '\n'
+    # 注意！
+    # 打印输出时会自动添加''/""作为字符串标识，其实变量中时不存在的
+    # 同样，当向文件中写入时，默认带着变量类型，如果单项写入，不存在多余的'[]'/'''问题，而如果整体写入一个列表或者字符串，则会带上类型字符
+    CERT52_LDAP_NOM.append(ldap_tmp)
+print '..<<LDAP规范化完毕>>..\n\n'
+
+
+
+print '....<<<<接下来开始为LDAP添加标号>>>>....\n\n'
+# 为了能够以数字标识顺序，先初始化存储四个层次组织名称的列表
+OS_Name_lst = [[] for i in range(4)]
+# 定义一个存储LDAP标号的列表
+CERT52_LDAP_ID = []
+for line in CERT52_LDAP_NOM:
+    ldap_part = line[0].split(':')
+    ldap_ID = []
+    i = 0
+    while i < 4:
+        if ldap_part[i] == ' ' or ldap_part[i] == '':
+            ldap_ID.append(0)
+            i += 1
+            continue
+        else:
+            if ldap_part[i] not in OS_Name_lst[i]:
+                OS_Name_lst[i].append(ldap_part[i])
+                ldap_ID.append(OS_Name_lst[i].index(ldap_part[i]) + 1)
+                i += 1
+                continue
+            else:
+                ldap_ID.append(OS_Name_lst[i].index(ldap_part[i]) + 1)
+                i += 1
+                continue
+    tmp_0 = []
+    tmp_0.append(line)
+    tmp_0.append(ldap_ID)
+    CERT52_LDAP_ID.append(tmp_0)
+print '....<<<<CERT5.2 LDAP数据结构化标记ID完成>>>>....\n\n'
+for i in range(len(CERT52_LDAP_ID)):
+    print i, CERT52_LDAP_ID[i][0], ' : \t', CERT52_LDAP_ID[i][1], '\n'
+    i += 1
+
+print '..<<将规范化的LDAP标记写入文件>>..\n\n'
+f_0 = open(r'CERT5.2_LDAP_ID.csv', 'w')
+for i in range(len(CERT52_LDAP_ID)):
+    #print i, CERT52_LDAP_ID[i][0], ' : \t', CERT52_LDAP_ID[i][1], '\n'
+    for ele in CERT52_LDAP_ID[i][0]:
+        # 原始LDAP数据
+        f_0.write(ele)
+        f_0.write(',')
+    for ele in CERT52_LDAP_ID[i][1]:
+        f_0.write(str(ele))
+        f_0.write(',')
+    f_0.write('\n')
+    i += 1
+
+
+
+print '....<<<<得到了LDAP规范化标记，接下来结合用户LDAP数据和心理数据生成其最终的关系特征>>>>....\n\n'
+# 组织结构数据LDAP
+# 首先定义了三个用于保存用户的Big5-LDAP特征的文件，分别保存全体、第一事业部、第二事业部的数据
+f_1 = open('CERT5.2_Big5_LDAP_All_Feats.csv', 'w')
+f_2 = open('CERT5.2_Big5_LDAP_Part1_Feats.csv', 'w')
+f_3 = open('CERT5.2_Big5_LDAP_Part2_Feats.csv', 'w')
+# 定义对应于上述文件的数据列表
+CERT52_All_Feats = []
+CERT52_Part1_Feats = []
+CERT52_Part2_Feats = []
+
+for line in f_big5_lst:
+    # UserID, O, C, E, A, N, ID1, ID2, ID3, ID4
+    # employee_name,user_id,O,C,E,A,N
+    # Maisie Maggy Kline,MMK1532,17,17,16,22,28
+    # 首先输入OCEAN心理特征
+    tmp_1 = []
+    line_lst = line.strip('\n').strip(',').split(',')
+    if line_lst[1] == 'user_id':
+        continue
+    #print 'line_lst is ', line_lst, '\n'
+    tmp_1.append(line_lst[1])
+    tmp_1.append(float(line_lst[2]))
+    tmp_1.append(float(line_lst[3]))
+    tmp_1.append(float(line_lst[4]))
+    tmp_1.append(float(line_lst[5]))
+    tmp_1.append(float(line_lst[6]))
+    # 接着需要输入LDAP特征
+    # 首先需要从LDAP_ID数据中定位
+    j = 0
+    while j < len(CERT52_LDAP_ID):
+        # 1 - Executive: 1 - Adminstration: : ,BBB0012,CWC0006,1,1,0,0,
+        # CERT52_LDAP_ID[j][0] =
+        # ['1 - Executive: 1 - Adminstration: 2 - EmployeeRelations: ', 'KMM0014', 'NMM0026', 'TIR0025', 'UAR0024']
+        if line_lst[1] in CERT52_LDAP_ID[j][0]:
+            index_1 = j
+            break
+        else:
+            j += 1
+            continue
+    for ele in CERT52_LDAP_ID[index_1][1]:
+        tmp_1.append(float(ele))
+    # 将得到的feats数据写入到对应的用户列表
+    CERT52_All_Feats.append(tmp_1)
+    if '1 - Executive' in CERT52_LDAP_ID[index_1][0][0]:
+        # 这是第一事业部
+        CERT52_Part1_Feats.append(tmp_1)
+    if '2 - Executive' in CERT52_LDAP_ID[index_1][0][0]:
+        CERT52_Part2_Feats.append(tmp_1)
+print '....<<<<CERT5.2的用户的OCEAN与LDAP数据关联特征提取完毕，准备写入文件>>>>....\n\n'
+
+
+
+print '....<<<<将用户的OCEAN数据与LDAP特征写入对应文件>>>>....\n\n'
+for line in CERT52_All_Feats:
+    for ele in line:
+        f_1.write(str(ele))
+        f_1.write(',')
+    f_1.write('\n')
+
+for line in CERT52_Part1_Feats:
+    for ele in line:
+        f_2.write(str(ele))
+        f_2.write(',')
+    f_2.write('\n')
+
+for line in CERT52_Part2_Feats:
+    for ele in line:
+        f_3.write(str(ele))
+        f_3.write(',')
+    f_3.write('\n')
+
+f_1.close()
+f_2.close()
+f_3.close()
+
+print '....<<<<用户OCEAN与LDAP特征数据写入完毕>>>>....\n\n'
+
+
+
+
+
+
+
+
+sys.exit()
---
+++ PythonCode0/DBSCAN_Module.py
@@ -0,0 +1,86 @@
+# coding:utf-8
+# 由于需要对1000-2000个用户特征进行聚类，而如果直接使用KMeans聚类则无法很好确定K值遍历的上限
+# 故首先采用尝试使用密度聚类DBSCAN来发现其中的核心（聚类中心），然后再使用KMeans聚类
+
+import numpy as np
+import sklearn.cluster as skc
+import sklearn.preprocessing as skp
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import sys, os
+
+
+
+def DBSCAN_Clustering(Data_lst, EPS, MiniSamples):
+    # Data_lst应该为一个原始纯碎的数值数组
+    X = np.array(Data_lst)
+    db = skc.DBSCAN(eps=EPS, min_samples=MiniSamples).fit(X)
+    # 和X同一个维度，labels对应索引序号的值 为她所在簇的序号。若簇编号为-1，表示为噪声
+    labels = db.labels_
+
+    print ('每个样本的簇标号：\n')
+    print labels, '\n'
+
+    ratio = len(labels[labels[:] == -1]) / len(labels) # 计算噪声点个数占综述比例
+    print '噪声比： ', ratio, '\n'
+
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0) # 获取分簇数目
+    n_core_ = len(db.core_sample_indices_)
+    print '总共识别群簇数目为： ', n_clusters_, '\n'
+    print '总共识别核心点为： ', n_core_, '\n'
+
+    if len(set(labels)) == 1:
+        score = 0.0
+        return n_clusters_, n_core_, labels, score
+    score = metrics.silhouette_score(X, labels) # [-1,1]，1最好，-1最差
+    print '轮廓系数指标为： ', score, '\n'
+
+    for i in range(n_clusters_):
+        print '群簇 ', i, ' 所有样本为： \n'
+        one_cluster = X[labels == i]
+        print one_cluster, '\n'
+        plt.plot(one_cluster[:, 0], one_cluster[:, 1], 'o')
+    #plt.show()
+    return n_clusters_, n_core_, labels, score
+
+# 指定数据源
+f_0 = open('CERT5.2_Big5_LDAP_All_Feats.csv', 'r')
+f_0_lst = f_0.readlines()
+f_0.close()
+
+# 读取数据源，输入
+# 数据样例：
+# MMK1532,17.0,17.0,16.0,22.0,28.0,2.0,5.0,20.0,40.0,
+Data_lst = []
+Users_lst = []
+for line in f_0_lst[:]:
+    line_lst = line.strip('\n').strip(',').split(',')
+    Users_lst.append(line_lst[0])
+    tmp_0 = []
+    print line_lst[1:], '\n'
+    for ele in line_lst[1:]:
+        tmp_0.append(float(ele))
+    Data_lst.append(tmp_0)
+
+# 将数据送入DBSCAN聚类
+Data_lst = skp.MinMaxScaler().fit_transform(Data_lst)
+Choice_lst = []
+for minisample in range(2,20,1):
+    for eps in range(1,100,2):
+        eps_0 = float(eps) / 100
+        N_Clusters, N_Cores, Labels, Score = DBSCAN_Clustering(Data_lst, eps_0, minisample)
+        tmp_1 = []
+        tmp_1.append(N_Clusters)
+        tmp_1.append(N_Cores)
+        tmp_1.append(Labels)
+        tmp_1.append(Score)
+        Choice_lst.append(tmp_1)
+b = sorted(Choice_lst, key=lambda t:t[3], reverse=True)
+for i in range(10):
+    print i, '\n'
+    for ele in b[i]:
+        print ele, '\n'
+    print '\n\n'
+
+
+sys.exit()
